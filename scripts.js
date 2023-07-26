83c5a21b90fd87696bac2fd80ee0df25ba1308d4
Rafael H Gomes <rafaelhgomes15@gmail.com>
2023-07-26 00:11:28 -0300

    finished project

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ const menuSection = document.querySelector('.menu-section');
 const menuMobile = menuSection.querySelector('.menu-mobile');
 
 menuMobile.addEventListener('click', () => {
+
+  document.body.style.overflow = show ? 'hidden' : 'initial';
+
   menuSection.classList.toggle('on', show);
   show = !show;
 })
